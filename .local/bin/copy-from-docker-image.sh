75fa89bf715eaea6e2c8d37fcdf2c7b6e99758a3
fluxfw <fluxfw@fluxlabs.ch>
2022-03-07 14:18:54 +0100

    fix

--- .local/bin/copy-from-docker-image.sh
+++ .local/bin/copy-from-docker-image.sh
@@ -2,11 +2,28 @@
 
 set -e
 
-if [ -d "$3" ] || [ -f "$3" ]; then
-  echo "$3 exists"
-  exit 1
+image="$1"
+if [ -z "$image" ]; then
+    echo "Please pass an image"
+    exit 1
 fi
 
-mkdir -p "`dirname "$3"`"
+path_in_image="$2"
+if [ -z "$path_in_image" ]; then
+    echo "Please pass a path in image"
+    exit 1
+fi
 
-docker run --rm -it -u `id -u`:`id -g` -v "$PWD":/copy-from-docker-image-host -w /copy-from-docker-image-host --entrypoint cp "$(echo -e "FROM alpine:latest\nCOPY --from=\"$1\" \"$2\" /copy-from-docker-image" | docker build - --pull | awk '/Successfully built/{print $NF}')" -r /copy-from-docker-image "$3"
+local_path="$3"
+if [ -z "$local_path" ]; then
+    echo "Please pass a local path"
+    exit 1
+fi
+if [ -d "$local_path" ] || [ -f "$local_path" ]; then
+    echo "Please remove existing $local_path"
+    exit 1
+fi
+
+mkdir -p "`dirname "$local_path"`"
+
+docker run --rm -it -u `id -u`:`id -g` -v "$PWD":/copy-from-docker-image-host -w /copy-from-docker-image-host --entrypoint cp "$(echo -e "FROM alpine:latest\nCOPY --from=\"$image\" \"$path_in_image\" /copy-from-docker-image" | docker build - --pull | awk '/Successfully built/{print $NF}')" -r /copy-from-docker-image "$local_path"

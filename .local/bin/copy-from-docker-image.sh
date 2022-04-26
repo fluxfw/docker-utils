ca8d345d15175739a3dc3eba7212ec99a820615b
fluxfw <fluxfw@fluxlabs.ch>
2022-04-26 12:32:19 +0200

    local dependency

--- .local/bin/copy-from-docker-image.sh
+++ .local/bin/copy-from-docker-image.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-if [ -z `command -v run-in-docker` ]; then
-    echo "Please install flux-docker-utils"
-    exit 1
-fi
-
 image="$1"
 if [ -z "$image" ]; then
     echo "Please pass an image"
@@ -32,4 +27,4 @@ fi
 mkdir -p "`dirname "$local_path"`"
 
 echo "Copy $path_in_image from $image to $local_path"
-run-in-docker "$(echo -e "FROM alpine:latest\nCOPY --from=\"$image\" \"$path_in_image\" /copy-from-docker-image" | docker build - --pull | awk '/Successfully built/{print $NF}')" cp -r /copy-from-docker-image "$local_path"
+"$(dirname "`realpath "$0"`")/run-in-docker.sh" "$(echo -e "FROM alpine:latest\nCOPY --from=\"$image\" \"$path_in_image\" /copy-from-docker-image" | docker build - --pull | awk '/Successfully built/{print $NF}')" cp -r /copy-from-docker-image "$local_path"

307aea4e4c2eb1ac6aa9dd44cf4685b8b438dec2
fluxfw <fluxfw@fluxlabs.ch>
2022-04-26 13:33:09 +0200

    only use tty if available

--- .local/bin/run-in-docker.sh
+++ .local/bin/run-in-docker.sh
@@ -18,5 +18,10 @@ shift
 
 #docker pull "$image"
 
-folder=`basename "$PWD"`
-docker run --rm -it -u `id -u`:`id -g` -v "$PWD":"/code/$folder" -w "/code/$folder" $(printenv | grep "FLUX_" | cut -f1 -d= | sed 's/^/-e /') --entrypoint "$entrypoint" "$image" "$@"
+folder="`basename "$PWD"`"
+
+if [ -n "$SHELL" ]; then
+    it="-it"
+fi
+
+docker run --rm $it -u `id -u`:`id -g` -v "$PWD":"/code/$folder" -w "/code/$folder" $(printenv | grep "FLUX_" | cut -f1 -d= | sed 's/^/-e /') --entrypoint "$entrypoint" "$image" "$@"

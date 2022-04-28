1de5e359a531a8981601802157d10a3fe54ae722
fluxfw <fluxfw@fluxlabs.ch>
2022-04-28 10:33:22 +0200

    remove build

--- .local/bin/copy-from-docker-image.sh
+++ .local/bin/copy-from-docker-image.sh
@@ -29,4 +29,4 @@ mkdir -p "`dirname "$local_path"`"
 folder="/code/$local_path"
 
 echo "Copy $path_in_image from $image to $local_path"
-docker run --rm -it --network none -u `id -u`:`id -g` -v "$PWD/$local_path":"$folder" "$(echo -e "FROM alpine:latest\nCOPY --from=\"$image\" \"$path_in_image\" /copy-from-docker-image" | docker build - --pull | awk '/Successfully built/{print $NF}')" cp -r /copy-from-docker-image "$folder"
+docker run --rm -it --network none -u `id -u`:`id -g` -v "$PWD":"$folder" "$(echo -e "FROM alpine:latest\nCOPY --from=\"$image\" \"$path_in_image\" /copy-from-docker-image" | docker build - --pull | awk '/Successfully built/{print $NF}')" cp -r /copy-from-docker-image "$folder/$local_path"

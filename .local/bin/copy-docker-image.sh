bcca0cec53d025f6420d2da5150f13d02c97f3d3
fluxfw <fluxfw@fluxlabs.ch>
2022-11-15 15:13:14 +0100

    copy-from-docker-image

---
+++ .local/bin/copy-docker-image.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+set -e
+
+from_image="$1"
+if [ -z "$from_image" ]; then
+    echo "Please pass a from image"
+    exit 1
+fi
+
+to_image="$2"
+if [ -z "$to_image" ]; then
+    echo "Please pass a to image"
+    exit 1
+fi
+
+#docker pull "$from_image" --all-tags
+
+for tag in $(docker images "$from_image" | cut -d " " -f 4); do docker tag "$from_image:$tag" "$to_image:$tag"; done
+
+#docker push "$to_image" --all-tags
